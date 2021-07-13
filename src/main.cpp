e0305a925f5f92b5d22df3af151d5407590f0436
Natalie Cygan <nataliemcygan@gmail.com>
2021-07-13 15:01:34 -0700

    initial code to test all motors

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include "Arduino.h"
 
+const int MOTOR_PINS[12] = {12, A0, A1, A2, A3, A4, A5, 5, 6, 9, 10, 11};
+
 void 
 setup () {
+    for (int i = 0; i < 12; i++) {
+        pinMode(MOTOR_PINS[i], OUTPUT);
+        digitalWrite(MOTOR_PINS[i], LOW);
+    }
+
 }
 
 void 
 loop () {
-    Serial.println("Hello");
+    for (int i = 0; i < 12; i++) {
+        digitalWrite(MOTOR_PINS[i], HIGH);
+        delay(100);
+        digitalWrite(MOTOR_PINS[i], LOW);
+        delay(50);
+        digitalWrite(MOTOR_PINS[i], HIGH);
+        delay(50);
+        digitalWrite(MOTOR_PINS[i], LOW);
+        delay(50);
+
+        delay(500);
+    }
 }
