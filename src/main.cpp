599c68a883cbbe94be77c33e3e34bac344e959af
Natalie Cygan <nataliemcygan@gmail.com>
2021-07-14 21:15:03 -0700

    Correct motor turns on based on direction user is facing

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_Sensor.h>
 
 Adafruit_LIS3MDL lis3mdl;   // magnetometer
+int prev_bin;
 
 const int MOTOR_PINS[12] = {12, A0, A1, A2, A3, A4, A5, 5, 6, 9, 10, 11};
 
@@ -54,6 +55,22 @@ loop () {
     Serial.print(" ");
     Serial.print("Compass Heading: ");
     Serial.print(heading);
+    Serial.print(" ");
+
+    int bin = ((int) heading) / 30;
+
+    Serial.print("Bin: ");
+    Serial.print(bin);
+
+    for (int i = 0; i < 12; i++) {
+
+        if (i == bin) {
+            digitalWrite(MOTOR_PINS[i], HIGH);
+        } else {
+            digitalWrite(MOTOR_PINS[i], LOW);
+        }
+    }
+    
 
     // for (int i = 0; i < 12; i++) {
         // digitalWrite(MOTOR_PINS[i], HIGH);
