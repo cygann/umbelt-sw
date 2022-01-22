5796090931d136aab181297c1f3c8eb5855529ee
Natalie Cygan <nataliemcygan@gmail.com>
2022-01-22 13:39:00 -0800

    cleanup

--- src/haptics.h
+++ src/haptics.h
@@ -16,20 +16,5 @@ const int M_RANGE_END = N_MOTORS;
 
 void init_haptics();
 
-/* --------- Primary Haptic Functions ---------- */
-void turn_on_motor_set(bool motors[], int value);
-void turn_off_motor_set(bool motors[]);
-void vibrate_single_motor(int loc, int dur);
-void turn_off_all_motors();
-
-/* --------- Specialty Haptics ---------- */
-void triple_buzz(bool motors[]);
-void triple_buzz_single_loc(int loc);
-void surround_blink();
-
-/* --------- Haptic Utility functions ---------- */
-bool is_back_facing(int loc);
-
 /* --------- Test & Debug functions ---------- */
 void haptics_test();
-void haptics_test_multi();
--- src/main.cpp
+++ src/main.cpp
@@ -33,33 +33,12 @@ setup () {
     // Init umbelt modules
     init_haptics();
     init_indicator_led();
-
-    haptics_test();
-    // compass = init_compass();
-
-    // Neopixel init
-    // pinMode(BAT_PIN, INPUT);
-    // pixels.begin();
-    // pixels.setBrightness(5);
-    // pixels.show();   // Send the updated pixel colors to the hardware.
-
-    // Init bluetooth modules
-    // init_bluetooth(&ble);
 }
 
 void
 loop () {
-    // haptics_test();
-    // set_battery_led();
-    // analog_test();
-    // haptics_test_multi();
-    // haptics_test();
+    haptics_test();
 
-    // compass_update(&compass);
-    // surround_blink();
-    // read_gyro(&compass);
-
-    // bluetooth_read(&ble);
 }
 
 void
@@ -67,25 +46,3 @@ init_indicator_led() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 }
-
-/* Adafruit feathersense neopixel
-void
-set_battery_led() {
-    // Calculate battery voltage
-    int voltage_raw = analogRead(BAT_PIN);
-    // Serial.print("Voltage raw: ");
-    // Serial.print(voltage_raw);
-    // Serial.println("");
-    float voltage = (voltage_raw / 1023.0) * 2 * 3.3;
-
-    // Set Pixel color based on battery charge
-    if (voltage <= 3.4) {
-        pixels.setPixelColor(0, 120, 0, 0);
-    } else if (voltage > 3.4 && voltage <= 3.8) {
-        pixels.setPixelColor(0, 30, 120, 0);
-    } else {
-        pixels.setPixelColor(0, 0, 180, 0);
-    }
-    pixels.show();   // Send the updated pixel colors to the hardware.
-}
-*/
