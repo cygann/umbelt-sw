068c461b9d60551402f5e65282f8c4a3744565de
Jason Lin <jclin2.2009@gmail.com>
2022-05-21 16:04:45 -0700

    pulse library works

--- src/haptics.h
+++ src/haptics.h
@@ -31,5 +31,79 @@ void init_haptics();
 void haptics_test();
 void control_device(int duration, double percent_motor, int num_cycles);
 void actuate_motor(int motor_pin, int duration, double percent_motor);
-void run_haptics(pulse* p, int num_cycles);
-void actuate_by_freq(int motor_pin, int duration, double percent_motor, int frequency);
+void run_haptics(pulse* p, int num_pulses, int num_cycles);
+void actuate_hz(int motor_pin, int duration, double percent_motor, float frequency);
+
+/* --------- Haptic Sample Library ---------- */
+static float noteFrequenciesExt[11] {98, 110, 123, 131, 147, 165, 175, 196, 220, 247, 261}; // G2 --> C4
+
+static pulse jackhammer[2] = {
+  {150, 100},
+  {0, 100}
+};
+
+static pulse bach[32] = {
+  {noteFrequenciesExt[0], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[8], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[0], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[8], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[4], 100},
+  {noteFrequenciesExt[1], 100},
+  {noteFrequenciesExt[5], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[5], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[5], 100},
+  {noteFrequenciesExt[1], 100},
+  {noteFrequenciesExt[5], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[9], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[5], 100},
+  {noteFrequenciesExt[10], 100},
+  {noteFrequenciesExt[5], 100}
+};
+static pulse maryHadALittleLamb [29] = {
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[3], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {0, 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {0, 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[7], 1000},
+  {noteFrequenciesExt[7], 1000},
+  {0, 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[3], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[5], 1000},
+  {noteFrequenciesExt[4], 1000},
+  {noteFrequenciesExt[3], 1000},
+};
